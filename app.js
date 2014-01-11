4f0cc1a6054b21ba42472a218ff042d4dff9cd99
baufaker <baufaker@gmail.com>
2014-01-11 00:16:59 -0200

    Primeiro commit. Projeto produzo apenas com rotas e jade básico

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var routes = require('./routes/rotas.js');//quando dá require nessa pasta, puxa, automaticamente, o módulo index.js. Se quiser puxar outros módulos, tem que especificá-los. Ex: ./routes/module.js
+var app = express();
+
+//Rotas
+
+//dá get em '/'(root) e, como callback, puxa a função 'index' exportada do arquivo 'index.js' que está em require './routes'.
+
+app.get('/', routes.index);
+
+app.get('/about', routes.about);
+
+app.get('/contact', routes.contact);
+
+//Set 404 route to node.js app
+
+app.listen(8080);
+console.log('Servidor rodando em localhost, porta 8080');
---
+++ routes/rotas.js
@@ -0,0 +1,26 @@
+fs = require('fs');
+
+//Utilizando layouts .jade
+
+//Aparentemente, o res.render já cria um valor de statusCode para o response do HTTP (no caso, com código 200, se não houver erro). Para testar, pode utilizar res.render('layout.jade', {'title': res.statusCode});
+
+exports.index = function(req, res){
+	res.render('layout.jade', {
+		'title': 'Home', 
+		'headTitle': 'Produzo'
+	});
+}
+
+exports.contact = function(req, res){
+	res.render('layout.jade', {
+		'title': 'Contact',
+		'headTitle': 'Entre em contato!'
+	});
+}
+
+exports.about = function(req, res){
+	res.render('layout.jade', {
+		'title': 'About',
+		'headTitle': 'Sobre o Produzo'
+	});
+}
