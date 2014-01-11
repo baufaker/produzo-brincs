5cc2846650126c8ceb4dfff7cef66469e1b781f8
baufaker <baufaker@gmail.com>
2014-01-11 00:46:57 -0200

    arrumei o content block do layout.jade e agora todos os templates puxam o layout. =)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var routes = require('./routes/rotas.js');//quando dá require nessa pasta, puxa, automaticamente, o módulo index.js. Se quiser puxar outros módulos, tem que especificá-los. Ex: ./routes/module.js
 var app = express();
 
+
 //Rotas
 
 //dá get em '/'(root) e, como callback, puxa a função 'index' exportada do arquivo 'index.js' que está em require './routes'.
--- routes/rotas.js
+++ routes/rotas.js
@@ -5,21 +5,21 @@ fs = require('fs');
 //Aparentemente, o res.render já cria um valor de statusCode para o response do HTTP (no caso, com código 200, se não houver erro). Para testar, pode utilizar res.render('layout.jade', {'title': res.statusCode});
 
 exports.index = function(req, res){
-	res.render('layout.jade', {
+	res.render('index.jade', {
 		'title': 'Home', 
-		'headTitle': 'Produzo'
+		'headTitle': 'Produzo',
 	});
 }
 
 exports.contact = function(req, res){
-	res.render('layout.jade', {
+	res.render('contact.jade', {
 		'title': 'Contact',
 		'headTitle': 'Entre em contato!'
 	});
 }
 
 exports.about = function(req, res){
-	res.render('layout.jade', {
+	res.render('about.jade', {
 		'title': 'About',
 		'headTitle': 'Sobre o Produzo'
 	});
